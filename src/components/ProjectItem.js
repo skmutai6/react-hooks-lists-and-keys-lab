6f4e3f8b3d45ecd3cfb7522d1f6e51d5708270ee
skmutai6 <skmutai6@gmail.com>
2024-04-24 22:25:33 +0300

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,20 @@
 import React from "react";
+import user from "../data/user";
 
 function ProjectItem({ name, about, technologies }) {
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        <span />
+
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,12 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        <ProjectItem name={projects[0].name} about={projects[0].about} />
+        <ProjectItem name={projects[1].name} about={projects[1].about} />
+        <ProjectItem name={projects[2].name} about={projects[2].about} />
+      </div>
     </div>
   );
 }
